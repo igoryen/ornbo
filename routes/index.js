d5edb4cb9ebd96c46cb0ad5e385549f9e8da3c77
igoryen <igor.yentaltsev@gmail.com>
2016-11-20 17:59:33 -0500

    Added the /about route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ornbo' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Ornbo' });
+});
+
 module.exports = router;
