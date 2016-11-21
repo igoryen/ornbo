6c78141b9a6aa1802a9f826e97a652084015e30a
igoryen <igor.yentaltsev@gmail.com>
2016-11-20 20:36:53 -0500

    Added a local variable with /added route

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,27 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Ornbo' });
 });
 
+// create GET
 router.get('/create', function(req, res, next) {
   res.render('create', { title: 'Ornbo' });
 });
-
+// create POST
 router.post('/create', function(req,res) {
     res.send(req.body);
 });
 
 router.get('/users/:id', function(req, res) {
     res.send('show content for user id' + req.params.id);
+});
+
+router.get('/added', function(req, res){
+    var user = {
+        first_name: 'Donald',
+        last_name: 'Trump',
+        address: 'The White House',
+        facebook_friends: '100000000'
+    }
+    res.render('added', {title: 'User', user: user});
 });
 
 
