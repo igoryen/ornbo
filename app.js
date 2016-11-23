176d3da2ef1238160c971e93dd97dace709088c3
igoryen <igor.yentaltsev@gmail.com>
2016-11-23 11:11:50 -0500

    Can create and retrieve tasks correctly

--- app.js
+++ app.js
@@ -42,8 +42,9 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true })); // #14
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,23 +1,44 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todo_development');
+mongoose.connect('mongodb://localhost:27017/todo_development');
 
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
-
+// #9
 var Task = new Schema({
-    task: String
+    task: String // #11
 });
-var Task = mongoose.model('Task', Task);
+var Task = mongoose.model('Task', Task); // #10
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res) { // #13
     Task.find({}, function (err, docs){
         res.render('tasks/index', { 
             title: ' Ornbo | Todos index view' ,
-            docs: docs
+            docs: docs // #12 
         });
     });
+});
+
+// @4
+router.get('/new', function(req,res){ // #13
+    res.render('tasks/new.jade', {
+        title: 'New Task'
+    });
+});
+
+//#5
+router.post('/', function(req, res){ // #13
+    console.log("---->req.body.task: " + req.body.task);
+    var task = new Task(req.body.task); // #6
+    task.save(function(err){ // #7
+        if(!err){
+            res.redirect('/tasks'); // #8
+        } else {
+            res.redirect('/tasks/new');
+        }
+    });
+
 });
 
 module.exports = router;
